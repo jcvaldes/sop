965214b612cf6488beb5d6e50412c45b7049b1f9
Juan Carlos Valdés <idevkingos@gmail.com>
2020-09-21 23:50:51 -0300

    arquitectura base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://sop-central-back-sop-dev.noprod.correo.local/SOPCentral/services/rest/v1'
+  // apiUrl: 'http://sop-central-back-sop-dev.noprod.correo.local/SOPCentral/services/rest/v1'
+  apiUrl: 'sop-api.herokuapp.com'
 };
